9d2a66b629ba8df424e83e1766277e4d9af3a4a5
josephjensen <joe@cryptonumerics.com>
2022-10-07 14:49:40 -0700

    added elements to grab search parameters and fetch results from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 //  https://www.loc.gov/maps/?q=civil war&fo=json
+var submitButtonEl = $('#submit-button')
+var searchTextEl = $('#text-input')
+var dropDownEl = $('#dropdown')
+function fetchResults(event){
+    event.preventDefault()
+    var format = dropDownEl.find(":selected").attr("data-search-text")
+    fetch('https://www.loc.gov/' + format + '/?q=' + searchTextEl.val() + "&fo=json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+}
+
+submitButtonEl.on("click", fetchResults)
+
+
